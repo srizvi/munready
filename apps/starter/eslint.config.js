8eb2d78608baf8cf52149bbdeadee5f699e01452
Sam Rizvi <1440522+srizvi@users.noreply.github.com>
2025-10-10 04:19:19 -0700

    chore: remove nextjs #3

--- apps/starter/eslint.config.js
+++ apps/starter/eslint.config.js
@@ -1,14 +1,9 @@
 import baseConfig, { restrictEnvAccess } from "@momo/eslint-config/base";
-import nextjsConfig from "@momo/eslint-config/nextjs";
 import reactConfig from "@momo/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
-  {
-    ignores: [".next/**"],
-  },
   ...baseConfig,
   ...reactConfig,
-  ...nextjsConfig,
   ...restrictEnvAccess,
 ];
